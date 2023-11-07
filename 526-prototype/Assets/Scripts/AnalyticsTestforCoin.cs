6fad93a8c22d8ad0805e095a0d39401ccde342de
Delin "Angelina" Liang <66922835+Angelina1903@users.noreply.github.com>
2023-11-06 18:13:37 -0800

    Analytics New

--- 526-prototype/Assets/Scripts/AnalyticsTestforCoin.cs
+++ 526-prototype/Assets/Scripts/AnalyticsTestforCoin.cs
@@ -10,6 +10,12 @@ public class AnalyticsTestforCoin : MonoBehaviour
     [SerializeField] private string CoinsURL; //https://docs.google.com/forms/u/0/d/e/1FAIpQLSd33S19khipbCtDUnQYSfvidNHg8HBRK-BNOgA8WlDKU77QYg/formResponse
     [SerializeField] private string SpikeURL; //https://docs.google.com/forms/u/0/d/e/1FAIpQLSemguIxLw_fNak-MTN21GpAiJRyMkFSEYpxVbIvXPR9kwtn6Q/formResponse
     [SerializeField] private string TimeURL; //https://docs.google.com/forms/u/0/d/e/1FAIpQLSfDcgjOVmgtRhwrWizsM1YIhD_3l4LyiAXK_azBGMAWy7tuAg/formResponse
+    [SerializeField] private string CameraZoomURL; //https://docs.google.com/forms/u/0/d/e/1FAIpQLScW1tOg81aZAHJIiySJasMAdtSm15O-b_INHCfWTPWsCXPraQ/formResponse
+
+    [SerializeField] private bool EnableCoinsTest;
+    [SerializeField] private bool EnableSpikeTest;
+    [SerializeField] private bool EnabelTimeTest;
+    [SerializeField] private bool EnableCameraZoomTest;
 
     private long _sessionID;
     private int _testInt;
@@ -53,20 +59,34 @@ public class AnalyticsTestforCoin : MonoBehaviour
     {
         // Assign variables
 
-        StartCoroutine(PostCoin(_sessionID.ToString(), _testInt.ToString()));
+        if(EnableCoinsTest) StartCoroutine(PostCoin(_sessionID.ToString(), _testInt.ToString()));
     }
 
     public void SendSpike(float posX, float posY)
     {
-        Debug.Log("Sent Spike Form");
-        StartCoroutine(PostSpike(posX.ToString(), posY.ToString()));
+        if (EnableSpikeTest)
+        {
+            Debug.Log("Sent Spike Form");
+            StartCoroutine(PostSpike(posX.ToString(), posY.ToString()));
+        }
     }
 
     public void SendChckpointTime(float _Time)
     {
-        Debug.Log("Sent Checkpoint Time Form");
         float tempTime = UpdateTimeData(_Time);
-        StartCoroutine(PostCheckpointTime(_sessionID.ToString(), _testInt.ToString(), tempTime.ToString()));
+        if (EnabelTimeTest) {
+            Debug.Log("Sent Checkpoint Time Form");
+            StartCoroutine(PostCheckpointTime(_sessionID.ToString(), _testInt.ToString(), tempTime.ToString()));
+        }
+    }
+
+    public void SendCamera(float posX, float posY)
+    {
+        if (EnableCameraZoomTest)
+        {
+            Debug.Log("Sent Camera Form");
+            StartCoroutine(PostCamera(posX.ToString(), posY.ToString()));
+        }
     }
 
     private float UpdateTimeData(float _Time)
@@ -149,6 +169,32 @@ public class AnalyticsTestforCoin : MonoBehaviour
             else
             {
                 Debug.Log("Checkpoint Form upload complete!");
+
+            }
+
+        }
+    }
+
+    private IEnumerator PostCamera(string playerX, string playerY)
+    {
+        // Create the form and enter reponses;
+        WWWForm form = new WWWForm();
+        form.AddField("entry.1537586619", playerX);
+        form.AddField("entry.1193859103", playerY);
+
+        // Send responses and verify result
+        using (UnityWebRequest www = UnityWebRequest.Post(CameraZoomURL, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+
+            }
+            else
+            {
+                Debug.Log("Hinting Form upload complete!");
 
             }
 
